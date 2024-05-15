a6c25f46b4d00fcdf25b14e2f4f2bc51c851ff32
stivengarcia <js.garcia22@ciaf.edu.co>
2024-05-14 20:11:44 -0500

    actualizacion clase usuario 1

--- src/main/java/com/example/Usuario.java
+++ src/main/java/com/example/Usuario.java
@@ -1,28 +1,79 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Usuario {
+    private String nombre;
+    private String contrasena;
+    private static  HashMap<String, String> usuriosReguistrados = new HashMap<>();
 
-    private String nombreUsuario;
-    private double contraseña;
-
-    public Usuario(String nombreUsuario, double contraseña) {
-        this.nombreUsuario = nombreUsuario;
-        this.contraseña = contraseña;
-    }
-
-    public String getNombreUsuario(){
-        return nombreUsuario;
-    }
-    public void setNombreUsuario(){
-        this.nombreUsuario = nombreUsuario;
-    }
-    public double getContraseña(){
-        return contraseña;
+    public Usuario() {
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+        agregarUsuario(nombre,contrasena);
     }
 
-    public void setContraseña(double contraseña) {
-        this.contraseña = contraseña;
+    //metodos get y set para nombre y contraseña
+    public String getNombre() {
+        return nombre;
     }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    //metodo para agregar usuario al Hashmap
+    private  void agregarUsuario(String nombre, String contrasena){
+        usuriosReguistrados.put(nombre, contrasena);
+    }
+
+    //metodo para verificar contraseña
+    public boolean verificarContrasena(String contrasenaIngresada){
+        return  this.contrasena.equals(contrasenaIngresada);
+    }
+
+    public  static void  agregarNuevoUsuario(String nombre, String contrasena){
+        if (!usuriosReguistrados.containsKey(nombre)){
+            usuriosReguistrados.put(nombre, contrasena);
+            System.out.println(" Usuario agregado: " + nombre);
+        } else {
+            System.out.println("El usuario ya existe");
+        }
+    }
+
+    public  void crearUsuario(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Registro de nuevo usuario");
+        System.out.print("Ingrese el nombre de usuario: ");
+        String nombre = scanner.nextLine();
+
+
+        System.out.print("Ingrese la contraseña : ");
+        String contrasena = scanner.nextLine();
+
+
+        //  Se consume la nueva línea pendiente después de nextInt()
+        scanner.nextLine();
+
+        Usuario.agregarNuevoUsuario(nombre, contrasena);
+
+
+        scanner.close();
+
+    }
+
+
 }
+
 
 
