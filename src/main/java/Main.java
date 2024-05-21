e038e5ba0baba64d9ffab7c9433f4c34e5781767
Juantru05 <js.trujillo05@ciaf.edu.co>
2024-05-20 20:50:59 -0500

    se suben los metodos de la clase ventas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Ventas ventas=new Ventas();
-        Producto producto=new Producto();
+
 
 
 
--- src/main/java/Ventas.java
+++ src/main/java/Ventas.java
@@ -1,35 +1,61 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Ventas {
 
-    Producto producto = new Producto();
+    Producto producto=new Producto();
+
     private int idVenta;
     private double valorTotalVenta;
-    private ArrayList<Ventas> productosVendidos;
+    private ArrayList<Producto> productosVendidos;
     private Date fechaHora;
 
     public Ventas() {
 
         this.idVenta = idVenta;
         this.productosVendidos = new ArrayList<>();
+        this.valorTotalVenta=valorTotalVenta;
         this.fechaHora = new Date();
 
     }
 
-    public String agregarProductos() {
-
-    return null;
+    public int getIdVenta() {
+        return idVenta;
+    }
+    public List<Producto> getProductosVendidos() {
+        return productosVendidos;
     }
 
-
-    public double calcularValorTotal() {
-    return 0;
-
+    public double getValorTotalVenta() {
+        return valorTotalVenta;
     }
 
+    public Date getFechaHora() {
+        return fechaHora;
+    }
 
+    public void agregarProducto(Producto producto, int cantidad){
+
+        if(producto.getCantidadEnStock() >= cantidad){
+            producto.setCantidadEnStock(producto.getCantidadEnStock()-cantidad);
+            productosVendidos.add(producto);
+            valorTotalVenta += producto.getPrecio()*cantidad;
+        } else {
+            System.out.println("uy, parece que se nos agoto el producto "+producto.getNombre());
+        }
+    }
+
+    public double calcularValorTotal(){
+        valorTotalVenta=0.0;
+        for (Producto producto:productosVendidos){
+
+            valorTotalVenta+=producto.getPrecio();
+        }
+        return valorTotalVenta;
+    }
 }
 
 
